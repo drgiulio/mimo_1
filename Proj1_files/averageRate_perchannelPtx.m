9c96fb14f6199702e9892da4da372b00e357bc50
Giulio <giulio@Giulios-MBP.fritz.box>
2018-11-22 17:08:46 +0100

    PTasks 5 & 6 + Qs 11 & 12

--- Proj1_files/averageRate_perchannelPtx.m
+++ Proj1_files/averageRate_perchannelPtx.m
@@ -11,3 +11,28 @@ function [averageRate] = averageRate_perchannelPtx(Phi,averagePtx)
 % averageRate: maximum average achievable rate R
 
 % TODO
+
+% Number of channel realizations
+L = size(Phi,2);
+
+% Initialize
+Psi = zeros(size(Phi));
+K = zeros(1,L);
+
+% Compute waterfilling solution for every channel realization
+for l = 1:L
+    [Psi(:,l),~,K(l)] = waterfilling(sort(real(Phi(:,l)),'descend'),averagePtx);
+end
+
+% Compute average achievable rate
+%{
+rates = zeros(1,L);
+for l = 1:L
+    for k = 1:K(l)
+        rates(l) = rates(l) + max(log2(Phi(k,l)*(averagePtx+sum(Phi(1:K(l),l)))/K(l)), 0);
+    end
+end
+%}
+log_args = ones(size(Phi)) + Phi.*Psi;
+rates = sum(max(log2(log_args), 0), 1);
+averageRate = mean(rates);
